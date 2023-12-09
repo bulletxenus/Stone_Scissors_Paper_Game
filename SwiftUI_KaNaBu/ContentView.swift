fa39a04a279fbc5ce8915f3f1595598c5aa45598
Dzmitry Khamitsevich <dzmitry_khamitsevich@epam.com>
2023-12-09 09:48:58 +0200

    Initial Commit

---
+++ SwiftUI_KaNaBu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_KaNaBu
+//
+//  Created by Dzmitry Khamitsevich on 9.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_KaNaBu/SwiftUI_KaNaBuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_KaNaBuApp.swift
+//  SwiftUI_KaNaBu
+//
+//  Created by Dzmitry Khamitsevich on 9.12.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_KaNaBuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
